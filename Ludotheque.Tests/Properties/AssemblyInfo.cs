29a3753fe05b7b9f0ca04e0cf77a1e6f3de3f469
Mathieu Templier <templier.mathieu@gmail.com>
2017-03-06 14:23:45 +0100

    Change project to MVC + tests

--- Ludotheque.Tests/Properties/AssemblyInfo.cs
+++ Ludotheque.Tests/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // Les informations générales relatives à un assembly dépendent de 
 // l’ensemble d’attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
 // associées à un assembly.
-[assembly: AssemblyTitle("Ludotheque")]
+[assembly: AssemblyTitle("Ludotheque.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Ludotheque")]
+[assembly: AssemblyProduct("Ludotheque.Tests")]
 [assembly: AssemblyCopyright("Copyright ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // Le GUID suivant est pour l’ID de la typelib si ce projet est exposé à COM
-[assembly: Guid("2ebb7431-0e35-4cb4-baf8-3b0e7283fd3d")]
+[assembly: Guid("a7975af4-c26d-4353-b6c5-388ebe33319f")]
 
 // Les informations de version pour un assembly se composent des quatre valeurs suivantes :
 //
