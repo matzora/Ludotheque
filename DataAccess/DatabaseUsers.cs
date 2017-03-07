c6535a9e20bb50e453298a72bdbfb1685d1147c7
Mathieu Templier <templier.mathieu@gmail.com>
2017-03-07 10:25:30 +0100

    database connection users

---
+++ DataAccess/DatabaseUsers.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class DatabaseUsers
+    {
+        public static void ADMIN_USER(out string username, out string password)
+        {
+            username = "admin";
+            password = "admin";
+        }
+
+        public static void LOWRIGHTS_USER(out string username, out string password)
+        {
+            username = "lowRight";
+            password = "lowRight";
+        }
+
+        public static void VIEWER_USER(out string username, out string password)
+        {
+            username = "viewer";
+            password = "viewer";
+        }
+    }
+}
